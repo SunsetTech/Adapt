ca2d6097276117c5f876f3b25dc8619ab87e0ed3
Sunset <777919+SunsetTech@users.noreply.github.com>
2026-07-20 06:03:24 -0400

    Adds basic unit tests in Tests/Composite/EOF.lua

--- Tests/Composite/EOF.lua
+++ Tests/Composite/EOF.lua
@@ -1,14 +1,16 @@
-local Adapt = require"Adapt"
-local Transform = Adapt.Transform
+local Null = require"Moonrise.Object.Null"
 local Stream = require"Moonrise.Stream"
 local Test = require"Moonrise.Test"
+local Adapt = require"Adapt"
+local Transform = Adapt.Transform
+local Wrapper = require"Adapt.Transform.Select.Wrapper"
 
 local TestGrammar = Transform.Grammar{
 	Anything = Transform.Bytes(1);
 	EOF = Transform.Lookahead("Negative", Transform.Jump"Anything");
 	Line = Transform.Sequence{
 		Transform.All(Transform.Jump"Anything");
-
+		Transform.String";";
 		Transform.Select{
 			Transform.String"\n";
 			Transform.Jump"EOF";
@@ -17,8 +19,47 @@ local TestGrammar = Transform.Grammar{
 	Transform.All(Transform.Jump"Line");
 }
 
+local TestPassingInput = Adapt.Execution.Bubble.Form(
+	{
+		Adapt.Execution.Bubble.Form("a","b","c");
+		";";
+		Wrapper(1, "\n")
+	},
+	{
+		Adapt.Execution.Bubble.Form("d","e","f");
+		";";
+		Wrapper(2, Null);
+	}
+)
+
+local TestFailingInput = Adapt.Execution.Bubble.Form(
+	{
+		Adapt.Execution.Bubble.Form("a","b","c");
+		";";
+		Wrapper(2, Null);
+	},
+	{
+		Adapt.Execution.Bubble.Form("d","e","f");
+		";";
+		Wrapper(1, "\n")
+	}
+)
+
 return Test.Series(
 	"EOF", {
-
+		Test.Passes(
+			"unparse success", function()
+				local Sink = Stream.String""
+				local Success = Adapt.Process(TestGrammar, "Lower", Sink, TestPassingInput)
+				assert(Success, "failed to unparse")
+			end
+		);
+		Test.Fails(
+			"fails with content after end", function()
+				local Sink = Stream.String""
+				local Success = Adapt.Process(TestGrammar, "Lower", Sink, TestFailingInput)
+				assert(Success)
+			end
+		)
 	}
 )
